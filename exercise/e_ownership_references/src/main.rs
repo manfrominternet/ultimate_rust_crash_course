4b4ee950ca7c3af79499436b628d6c80f0597389
textmanfrominternet <textamanfrominternet@gmail.com>
2025-02-24 14:26:56 +0100

    execrise 5!

--- exercise/e_ownership_references/src/main.rs
+++ exercise/e_ownership_references/src/main.rs
@@ -8,31 +8,20 @@ fn main() {
         println!("Please supply an argument to this program.");
         std::process::exit(-1);
     });
+    inspect(&arg);
 
-    // 1. Write a function `inspect` that takes a reference to a String, returns nothing, but
-    // prints whether the contents of the String is plural or singular. Then uncomment and run this
-    // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
-    // String reference
-    //
-    //inspect(&arg);
+ 
 
-    // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
-    // the String if it doesn't already end with "s". Then uncomment and run the code below with
-    // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
-    //
-    //change(&mut arg);
+
+    change(&mut arg);
     //println!("I have many {}", arg);
 
-    // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
-    // indicating whether or not the String both starts with a "b" AND contains an "a".
-    // Hint 1: use `.starts_with("b")` and `.contains("a")`
-    // Hint 2: `&&` is the boolean "AND" operator
-    //
-    //if eat(arg) {
-    //    println!("Might be bananas");
-    //} else {
-    //    println!("Not bananas");
-    //}
+
+    if eat(arg) {
+       println!("Might be bananas");
+    } else {
+       println!("Not bananas");
+    }
 
     // Try running this program with "boat", "banana", and "grapes" as the arguments :-)
 
@@ -48,3 +37,42 @@ fn main() {
     // bedazzle(&mut material);
     // println!("Wow! Now the material is `{}`!", material);
 }
+
+    // 1. Write a function `inspect` that takes a reference to a String, returns nothing, but
+    // prints whether the contents of the String is plural or singular. Then uncomment and run this
+    // code with `cargo run apple` and `cargo run apples'.  Hint: use `.ends_with("s")` on the
+    // String reference
+    //
+
+    fn inspect(s: &String) {
+        let msg = if s.ends_with('s') {
+            "plural"
+        } else {
+            "singular"
+        };
+
+        println!("{}", msg);
+    }
+
+    // 2. Write a function `change` that takes a *mutable* reference to a String and adds an "s" to
+    // the String if it doesn't already end with "s". Then uncomment and run the code below with
+    // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
+    //
+
+    fn change(s: &mut String) {
+        if !s.ends_with("s") {s.push_str("s")};
+
+        println!("{}", s);
+    }
+
+    // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
+    // indicating whether or not the String both starts with a "b" AND contains an "a".
+    // Hint 1: use `.starts_with("b")` and `.contains("a")`
+    // Hint 2: `&&` is the boolean "AND" operator
+    //
+
+    fn eat(s: String) -> bool {
+
+        if s.starts_with("b") && s.contains("a") { true } else { false }
+
+    }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
     println!("Hello, world!");
+    // let s1 = String::from("abc");
+    // let s2 = s1;
+    // println!("{}", s1); breaks! trying to use borrowed value
+    // Remedy!
+    //  let s1 = String::from("abc");
+    //  let s2 = s1.clone();
+    //  println!("{}", s1);
+
+    // let s1 = String::from("abc");
+    // do_stuff(s1);
+    // println!("{}", s1); FAILS  1
+
+    // let mut s1 = String::from("abc");
+    // s1 = do_stuff(s1);
+    // println!("{}", s1); WORKS 2
+
+    // now the proper way
+    let s1 = String::from("abc");
+    do_stuff(&s1); 
+}
+// fn do_stuff(s: String) {
+//     // do stuff
+// } 1
+
+// fn do_stuff(s: String) -> String {
+//     s
+// } 2
+
+// now the proper way
+fn do_stuff(s: &String) {
+
 }
